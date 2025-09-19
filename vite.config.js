383c6ef6819123fa8644db77184668f56b292ecf
Shreya Jaiswal <48164763+shreya-jaiswal@users.noreply.github.com>
2025-09-19 17:19:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/east-africa-bird-quiz/' // <-- set to '/<repo-name>/'
 })
